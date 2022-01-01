68472f88ef2fa8fda329ba5eb885552a270b6820
Wei Loong <nganweiloong@gmail.com>
2022-01-01 22:43:41 +0800

    Added loading.js and updated countrycontainer.js

--- src/Component/CountryContainer.js
+++ src/Component/CountryContainer.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Countrycard from "./Countrycard";
+import Loading from "./Loading";
 
 function CountryContainer() {
   const url = "https://restcountries.com/v3.1/all";
   const url2 = "https://jsonplaceholder.typicode.com/posts";
   const [countries, setCountries] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
   function compare(a, b) {
     if (a.name.common < b.name.common) {
       return -1;
@@ -24,6 +26,7 @@ function CountryContainer() {
       if (xhr.status == 200) {
         console.log("success");
         setCountries(xhr.response.sort(compare));
+        setIsLoaded(true);
       } else {
         console.log("error", xhr);
       }
@@ -33,12 +36,14 @@ function CountryContainer() {
 
     // some API require special authorization, that's when we use 'setRequestHeader'    xhr.send();
   }, []);
-  return (
+  return isLoaded ? (
     <div className="country-container">
       {countries.map((country) => {
         return <Countrycard {...country} />;
       })}
     </div>
+  ) : (
+    <Loading />
   );
 }
 
---
+++ src/Component/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loading.css";
+
+function Loading() {
+  return (
+    <div className="load-container">
+      <div class="loading"></div>;
+    </div>
+  );
+}
+
+export default Loading;
